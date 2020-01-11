fbe7d88014f02cf64a7024b8cfee6ed213bd3de1
Devon James Wright <devonjamess.wright@gmail.com>
2020-01-11 18:13:39 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
   def kind
     if @side_1 == @side_2 && @side_3 == @side_1 && @side_1 > 0 
       :equilateral
-    elsif @side_1 == @side_2 && @side_1 != @side_3 && @side_1 > 0 && @side_3 > 0 && (@side_1 + @side_2) > @side_3 or @side_1 == @side_3 && @side_1 != @side_2 && @side_1 > 0 && @side_2 > 0 && (@side_1 + @side_2) > @side_3 or @side_2 == @side_3 && @side_2 != @side_1 && @side_2 > 0 && @side_1 > 0 && (@side_2 + @side_3) > @side_1
+    elsif @side_1 == @side_2 && @side_1 != @side_3 && @side_1 > 0 && @side_3 > 0 && (@side_1 + @side_2) >= @side_3 or @side_1 == @side_3 && @side_1 != @side_2 && @side_1 > 0 && @side_2 > 0 && (@side_1 + @side_2) >= @side_3 or @side_2 == @side_3 && @side_2 != @side_1 && @side_2 > 0 && @side_1 > 0 && (@side_2 + @side_3) >= @side_1
       :isosceles
     elsif @side_1 != @side_2 && @side_1 != @side_3 && @side_3 != @side_2 && @side_1 > 0 && @side_2 > 0 && @side_3 > 0
       :scalene 
