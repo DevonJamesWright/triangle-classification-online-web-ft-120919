3680c57981f036dcb940ae77efdc04e04c2f9cc9
Devon James Wright <devonjamess.wright@gmail.com>
2020-01-11 18:10:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,12 +9,11 @@ class Triangle
   def kind
     if @side_1 == @side_2 && @side_3 == @side_1 && @side_1 > 0 
       :equilateral
-    elsif @side_1 == @side_2 && @side_1 + @side_2 > @side_3 && @side_1 > 0 && @side_3 > 0 or @side_1 == @side_3 && @side_1 != @side_2 && @side_1 > 0 && @side_2 > 0 or @side_2 == @side_3 && @side_2 != @side_1 && @side_2 > 0 && @side_1 > 0 
+    elsif @side_1 == @side_2 && @side_1 != @side_3 && @side_1 > 0 && @side_3 > 0 or @side_1 == @side_3 && @side_1 != @side_2 && @side_1 > 0 && @side_2 > 0 or @side_2 == @side_3 && @side_2 != @side_1 && @side_2 > 0 && @side_1 > 0 
       :isosceles
     elsif @side_1 != @side_2 && @side_1 != @side_3 && @side_3 != @side_2 && @side_1 > 0 && @side_2 > 0 && @side_3 > 0
       :scalene 
-    #elsif @side_1 + @side_2 < @side_3 or @side_2 + @side_3 < @side_1 or @side_1 + @side_3 < @side_2 or @side_1.abs != @side_1 or @side_2.abs != @side_2 or @side_3.abs != @side_3 or @side_1 == 0 or @side_2 == 0 or @side_3 == 0 
-   else 
+    elsif (@side_1 + @side_2) < @side_3 or (@side_2 + @side_3) < @side_1 or (@side_1 + @side_3) < @side_2 or @side_1.abs != @side_1 or @side_2.abs != @side_2 or @side_3.abs != @side_3 or @side_1 == 0 or @side_2 == 0 or @side_3 == 0 
       raise TriangleError
     end 
   end 
