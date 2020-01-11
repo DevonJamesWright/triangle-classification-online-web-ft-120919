3b8645facefb22c9fc5299b65b466b742f041dfc
Devon James Wright <devonjamess.wright@gmail.com>
2020-01-11 18:13:59 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
       :isosceles
     elsif @side_1 != @side_2 && @side_1 != @side_3 && @side_3 != @side_2 && @side_1 > 0 && @side_2 > 0 && @side_3 > 0
       :scalene 
-    elsif (@side_1 + @side_2) < @side_3 or (@side_2 + @side_3) < @side_1 or (@side_1 + @side_3) < @side_2 or @side_1.abs != @side_1 or @side_2.abs != @side_2 or @side_3.abs != @side_3 or @side_1 == 0 or @side_2 == 0 or @side_3 == 0 
+    elsif (@side_1 + @side_2) <= @side_3 or (@side_2 + @side_3) <= @side_1 or (@side_1 + @side_3) <= @side_2 or @side_1.abs != @side_1 or @side_2.abs != @side_2 or @side_3.abs != @side_3 or @side_1 == 0 or @side_2 == 0 or @side_3 == 0 
       raise TriangleError
     end 
   end 
